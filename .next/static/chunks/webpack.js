433ede155a20d376c6c7407252d6723af6fdec77
Ahmad Muhammad <theahmadm3@gmail.com>
2023-05-17 09:40:08 +0100

    added location button for Instructor

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2a70a33c5f8ffa6f"; }
+/******/ 		__webpack_require__.h = function() { return "bd55d38e0967591a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/Instructor.js
+++ src/pages/Instructor.js
@@ -1,52 +1,43 @@
 import "tachyons";
+import React, { useState } from 'react';
+import geolocation from 'geolocation';
 import QRCode from 'qrcode.react';
 
 function Instructor() {
-    const qrCodeValue = "h1";
-    const qrCodeValue2 = "h2";
-    const qrCodeValue3 = "h3";
-    const qrCodeValue4 = "h4";
-    const qrCodeValue5 = "h5";
+    const qrCodeValue = 'h1';
+    const [location, setLocation] = useState(null);
+
+    const handleLocationClick = () => {
+        geolocation.getCurrentPosition((error, position) => {
+            if (error) {
+                console.error('Error getting location:', error);
+            } else {
+                const { latitude, longitude } = position.coords;
+                setLocation({ latitude, longitude });
+                alert(`${position.coords.latitude} ${position.coords.longitude}`);
+            }
+        });
+    };
+
     return (
         <>
             <div className="pa2 center w-100 mt1 shadow-1 bg-transparent">
-                <header className="pa2 flex center flex-row">
-                    <h2 className="w-80 white tc center"> Welcome To Your Profile </h2>
-                </header>
-                <section className="flex flex-row pl2">
-                    <div className="w-60 center">
-                        <div className="pa2 shadow-1 grow ma2 bg-white">
-                            Instructor Name: <br />
-                            <br />
-                            Joe Schmoe
-                        </div>
-                        <div className="pa2 shadow-2 grow ma2 bg-white">
-                            ID: <br />
-                            <br />
-                            9119189110
-                        </div>
-                    </div>
-                </section>
                 <section className="shadow-1 pa2 mt4">
                     <h1 className="tc white"> Courses </h1>
                     <div className="scroll-x w-100 center scroll-y Icourse fit-content pa2 shadow-1 ma3 flex flex-row">
-                    <div className="pa3 ma2 bg-light-gray fit-content bn bg-white-70 flex flex-column shadow-1">
-                        <QRCode value={qrCodeValue} className="center" />
-                        <h2>
-                            CSC 101
-                        </h2>
-                        <p>
-                            Intro to computer science
-                        </p>
-                        <button disabled className="grow pa2 shadow-1 mt1"> Course materials </button>
-                    </div>
+                        <div className="pa3 ma2 bg-light-gray fit-content bn bg-white-70 flex flex-column shadow-1">
+                            <QRCode value={qrCodeValue} className="center" />
+                            <h2>CSC 101</h2>
+                            <p>Intro to computer science</p>
+                            <button onClick={handleLocationClick} className="grow pa2 shadow-1 mt1">
+                                Get Location
+                            </button>
+                        </div>
                     </div>
                 </section>
             </div>
         </>
     );
 }
-
-
 
 export default Instructor;
