61f5530cee3f6e8b320a8f5ee9d82e0d8839785d
Ahmad Muhammad <71970775+theahmadm3@users.noreply.github.com>
2023-06-24 14:53:39 +0100

    Added Students

--- src/data/students.js
+++ src/data/students.js
@@ -1,10 +1,34 @@
 export const students = [
     {
-        id: 1234567890,
-        password: "Password1",
-        fNmame: "first",
-        fLmame: "Last",
+        id: 10,
+        password: "P1",
+        fNmame: "Joe",
+        fLmame: "Schmoe",
+        type: 'STUD',
+        story: " My name is Joe Schmoe, a curious and enthusiastic student on a quest for knowledge and personal growth. From a young age, I have been captivated by the wonders of the world and the limitless possibilities that education holds."
+    },
+    {
+        id: 11,
+        password: "P2",
+        fNmame: "John",
+        fLmame: "Doe",
+        type: 'STUD',
+        story: " My name is John Doe, a curious and enthusiastic student on a quest for knowledge and personal growth. From a young age, I have been captivated by the wonders of the world and the limitless possibilities that education holds."
+    },
+    {
+        id: 12,
+        password: "P3",
+        fNmame: "Jane",
+        fLmame: "Smith",
         type: 'STUD',
         story: " My name is First Last, a curious and enthusiastic student on a quest for knowledge and personal growth. From a young age, I have been captivated by the wonders of the world and the limitless possibilities that education holds."
+    },
+    {
+        id: 13,
+        password: "P4",
+        fNmame: "Bob",
+        fLmame: "Robert",
+        type: 'STUD',
+        story: " My name is Bob Robert, a curious and enthusiastic student on a quest for knowledge and personal growth. From a young age, I have been captivated by the wonders of the world and the limitless possibilities that education holds."
     },
 ]
